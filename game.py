a657055352af7dad9258fadb2aed2bb4a737704d
ethan <ethanpeacock@aol.com>
2025-07-12 07:53:08 -0500

    add alphabet

--- game.py
+++ game.py
@@ -20,5 +20,7 @@
 
 # TODO: we need to define an alphabet and only take a through z characters reject evertying else
 
+alphabet = ['a', 'b', 'c', 'd',  'e', 'f', 'g',  'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 phrase = input("Enter hangman phrase: ")
 print(f"This is the user's phrase:\n{phrase}")
