2d3631177aad622e7b996f1e8ffc1120ba31c731
ethan <ethanpeacock@aol.com>
2025-07-12 11:59:10 -0500

    add some notes on how to animate the guessing view to the user

---
+++ testfiledrawdude.py
@@ -0,0 +1,38 @@
+import time
+import os
+
+
+
+
+
+dont let me die friend!
+  |--------\
+  |        [ ]
+  |        /|\
+__|___      |
+           / \      
+
+
+
+
+
+yo, so see that up there we need to animate based off of incorrect or correct guesses that dude right
+
+we have 1 2 3 4 5 6 7 placements being
+
+his head []
+his torso |
+his left arm /
+his right arm \
+his penis |
+his left leg /
+his right leg \ 
+
+right? so we need to just basically we can write it as a single string with spaces and \ns, we can store 7 strings in a list
+every incorrect guess iterates the animationaled hangman to the next node to the right this will give the appearance of drawing him from his 
+head torso leftarm right arm huge dick left leg and right leg sequentially right?
+
+we also need a string that is just the stand to hang him from right?
+so whoops we need 8 strings
+
+okay that should be pretty easy lets save these notes and add it to the game file next
