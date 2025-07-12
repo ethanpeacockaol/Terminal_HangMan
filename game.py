a7ae7e36b0ca72e78981996b8578478869e3e6cb
ethan <ethanpeacock@aol.com>
2025-07-12 07:48:19 -0500

    add todos to game.py

---
+++ game.py
@@ -0,0 +1,14 @@
+# Terminal Hangman
+
+# TODO: we need to capture the phrase from the user
+
+# TODO: we need to tell the user to pass the phone to their opponent
+
+# TODO: we need to get a letter guess from the oponent
+
+# TODO: if the letter is in the phrase we need to update the blank representations to include the correctly guessed letter
+
+# TODO: if the letter is not in the phrase we need to draws a  piece of the hangman and add the letter to the incorrect guesses list
+
+# TODO: we need to draw a blank represenetation of the phrase so the user can see the length of the statement as the play the game
+
