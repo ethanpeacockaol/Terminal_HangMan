0fcde3e10ff1bda8c4b44686a3b8068d3861a3cb
ethan <ethanpeacock@aol.com>
2025-07-13 00:28:28 -0500

    add todo for mistake missing noose base

--- hangman.py
+++ hangman.py
@@ -1,12 +1,14 @@
 hangmanfull = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n__|__       |\n           / \\"
 hangmanempty = "dont let me die friend!\n  |--------\\\n  |           \n  |           \n__|__"
 hangmanhead = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |           \n__|__"
-
 hangmanfirsttorso = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |         |\n"
+hangmanleftarm = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\n"
+hangmanrightarm = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n"
+hangmanfull = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n__|__       |\n           / \\"
 
-print(hangmanfirsttorso)
+print(hangmanrightarm)
 
-
+# TODO: whoops we forgot to draw the base on left arm torso and right arm thats okay we add it back quick keep goin
 # we wanna draw just the stand, then his head, then his first half of body, then left arm, then right arm, then lower half of body, then left leg, then right leg and he dead
 # 0 1 2 3 4 5 6 7
 # so its literally just 8 frames just fucking do it real quick dammintgod
