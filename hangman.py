5aef427a87596bc2ac812bfec527708dbc2e95a0
ethan <ethanpeacock@aol.com>
2025-07-13 00:21:58 -0500

    up to head string done

--- hangman.py
+++ hangman.py
@@ -1,8 +1,10 @@
 hangmanfull = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n__|__       |\n           / \\"
-
-
 hangmanempty = "dont let me die friend!\n  |--------\\\n  |           \n  |           \n__|__"
-print(hangmanempty)
+hangmanhead = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |           \n__|__"
+
+
+
+print(hangmanhead)
 
 
 # we wanna draw just the stand, then his head, then his first half of body, then left arm, then right arm, then lower half of body, then left leg, then right leg and he dead
