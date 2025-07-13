58c53f8845fa09019892f96f1ca6c13903e8509f
ethan <ethanpeacock@aol.com>
2025-07-13 00:20:03 -0500

    hey hangman empty string working cool

--- hangman.py
+++ hangman.py
@@ -1,2 +1,10 @@
 hangmanfull = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n__|__       |\n           / \\"
-print(hangmanfull)
+
+
+hangmanempty = "dont let me die friend!\n  |--------\\\n  |           \n  |           \n__|__"
+print(hangmanempty)
+
+
+# we wanna draw just the stand, then his head, then his first half of body, then left arm, then right arm, then lower half of body, then left leg, then right leg and he dead
+# 0 1 2 3 4 5 6 7
+# so its literally just 8 frames just fucking do it real quick dammintgod
