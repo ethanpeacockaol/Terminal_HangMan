b7921bad1d4805f4e94541bef5039fa6392dd415
ethan <ethanpeacock@aol.com>
2025-07-13 19:39:31 -0500

    add hangman frames to explode

--- explode.py
+++ explode.py
@@ -1,2 +1,24 @@
 # make explosion animation of hangman if they lose the game
 # add the song memory by cats after explosion
+
+import os
+import time
+
+
+
+
+
+hangmanempty = "dont let me die friend!\n  |--------\\\n  |           \n  |           \n__|__"
+hangmanhead = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |           \n__|__"
+hangmanfirsttorso = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |         |\n__|__"
+hangmanleftarm = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\n__|__"
+hangmanrightarm = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n__|__"
+hangmanlowertorso = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n__|__       |\n"
+hangmanleftleg = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n__|__       |\n           /"
+hangmanfull = "dont let me die friend!\n  |--------\\\n  |        [ ]\n  |        /|\\\n__|__       |\n           / \\"
+hangmanlist = [hangmanempty, hangmanhead, hangmanfirsttorso, hangmanleftarm, hangmanrightarm, hangmanlowertorso, hangmanleftleg, hangmanfull]
+
+for frame in hangmanlist:
+    print(frame)
+    time.sleep(2)
+    os.system("clear")
